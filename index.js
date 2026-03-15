328ce06d6c9fbc77cdaf358a1d6cd8757bfb0873
michaeltina <michaeltinashe00@gmail.com>
2026-03-15 21:07:20 +0200

    Fix the functionability of humanChoice and computerChoice

--- index.js
+++ index.js
@@ -2,29 +2,49 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    let choice = Math.floor(Math.random() * (max - min + 1) + min); 
+
+    if (choice === 1) {
+        return "rock"
+    }
+
+    else if (choice === 2) {
+        return 'paper'
+    }
+
+    else {
+        return 'scissors'
+    }
+    
 }
 
-let choice = getComputerChoice(1, 3)
+getComputerChoice(1, 3)
 
-if (choice === 1) {
-    console.log("rock")
-}
 
-else if (choice === 2) {
-    console.log('paper')
-}
-
-else {
-    console.log('scissors')
-}
 
 function getHumanChoice () {
-    let hChoice = prompt('Choose between rock, paper and scissors! ');
-
-    console.log(hChoice);
+    return prompt('Choose between rock, paper and scissors! ');
 }
+
+
 
 function playRound (humanChoice, computerChoice) {
-
+    if (humanChoice === computerChoice){
+        console.log("It's a tie! Try again")
+    } else if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'scissors' && computerChoice === 'paper') || (humanChoice === 'paper' && computerChoice === 'rock')
+    ) {
+        console.log('You Win')
+    } else {
+        console.log('Computer Wins')
+    } 
 }
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+
+playRound(humanChoice, computerChoice);
+
+console.log("Human chose:", humanChoice, "Type:", typeof humanChoice);
+console.log("Computer chose:", computerChoice, "Type:", typeof computerChoice);
